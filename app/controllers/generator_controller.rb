912189b2922a69d31b39b12edb7bfc4f952010eb
Michael Pierce <michael@affektive.com>
2014-07-01 12:47:13 -0600

    Preformance fixes

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -7,11 +7,6 @@ class GeneratorController < ApplicationController
     @URL_14 = 'https://www.parts-express.com/wired-home-skrl-14-100-14-awg-ofc-speaker-wire-100-ft--100-021'
     @URL_16 = 'https://www.parts-express.com/wired-home-skrl-16-100-16-awg-ofc-speaker-wire-100-ft--100-017'
     @URL_18 = 'https://www.parts-express.com/rca-ah18100sr-18-awg-speaker-wire-100-ft--100-130'
-    @AWG10 = LinkThumbnailer.generate(@URL_10)
-    @AWG12 = LinkThumbnailer.generate(@URL_12)
-    @AWG14 = LinkThumbnailer.generate(@URL_14)
-    @AWG16 = LinkThumbnailer.generate(@URL_16)
-    @AWG18 = LinkThumbnailer.generate(@URL_18)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Pictionary::Application.routes.draw do
 
   root 'generator#index'
   match '/index', to: 'generator#index', via: 'get'
-  match '/score', to: 'generator#score', via: 'get'
 
 end
