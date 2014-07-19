7ab273a4f26d516de5cd1828a042247bf21f81c3
Michael Pierce <michael@affektive.com>
2014-07-19 17:34:53 -0600

    Final fixes

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,12 +1,6 @@
 class GeneratorController < ApplicationController
-  include GeneratorHelper
 
   def index
-    @URL_10 = 'https://www.parts-express.com/wired-home-skrl-10-100-10-awg-ofc-speaker-wire-100-ft--100-029'
-    @URL_12 = 'https://www.parts-express.com/wired-home-skrl-12-100-12-awg-ofc-speaker-wire-100-ft--100-025'
-    @URL_14 = 'https://www.parts-express.com/wired-home-skrl-14-100-14-awg-ofc-speaker-wire-100-ft--100-021'
-    @URL_16 = 'https://www.parts-express.com/wired-home-skrl-16-100-16-awg-ofc-speaker-wire-100-ft--100-017'
-    @URL_18 = 'https://www.parts-express.com/rca-ah18100sr-18-awg-speaker-wire-100-ft--100-130'
   end
 
 end
