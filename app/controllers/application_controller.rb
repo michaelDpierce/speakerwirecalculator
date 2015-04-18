a21345dea4a5b4ea6e5ed288c4070c9c47db4e55
Michael Pierce <michael@affektive.com>
2015-04-18 13:17:06 -0600

    Remove some broken code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  APP_DOMAIN = 'www.speakerwirecalculator.com'
-  before_filter :ensure_domain
-
-  def ensure_domain
-    unless request.env['HTTP_HOST'] == APP_DOMAIN || Rails.env.development?
-      redirect_to "http://#{APP_DOMAIN}", :status =&gt; 301
-    end
-  end
 end
